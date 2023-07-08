88c716e243582ee2eb2b260b3f961727b08514a5
Roman Kryhul <104628639+UmyaFamilia@users.noreply.github.com>
2023-07-08 17:13:32 +0300

    homeWork number-7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,19 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
+const gallery = document.querySelector(".gallery");
 console.log(galleryItems);
+
+const array = galleryItems.map((item) => {
+  return `<li class="gallery__item">
+  <a class="gallery__link" href=${item.original}>
+     <img class="gallery__image"  src=${item.preview} alt=${item.description} " />
+  </a>
+</li>`;
+});
+
+gallery.insertAdjacentHTML("beforeend", array.join(""));
+var lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
